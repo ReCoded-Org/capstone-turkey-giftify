6e6770e3472f7329ab3f75d22f353f117511f524
CeyhunGulbas <cetree61@gmail.com>
2021-08-04 16:19:10 +0300

    Fix: yarn run fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route path="/" component={NavBar} />
-        {/* <Route exact path="/" component={Home} />
-        <Route path="/requests" component={Requests} />
-        <Route path="/donations" component={Donations} />
-        <Route path="/about" component={About} />
-        <Route path="/contactUs" component={ContactUs} />
-        <Route path="/" component={} /> */}
-      </Router>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
