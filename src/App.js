d338acea9afc1642ad47bf1c9cd5766c44043d16
demirbileku <udemirbilek2@gmail.com>
2021-08-12 21:17:07 +0300

    Build: add footer to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "./App.css";
-import HomePage from "./containers/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Container>
-        <HomePage />
+        <Footer />
       </Container>
     </>
   );
