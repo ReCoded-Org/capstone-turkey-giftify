0ae1d886486adf228d0491e96a63c438a23d87e2
CeyhunGulbas <cetree61@gmail.com>
2021-08-14 02:10:59 +0300

    Build: add ourTeam

---
+++ src/components/OurTeam/OurTeam.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const OurTeam = () => {
+
+
+    return(
+        <div className="ourTeamMainDiv">
+            <h1>Our Team</h1>
+            <div className="teamMembersCardMainDiv">
+                
+            </div>
+        </div>
+    )
+
+}
+
+export default OurTeam
---
+++ src/containers/OurTeam/ourTeamData.js
@@ -0,0 +1,7 @@
+export const ourTeamData = [
+    {
+      image: "imgURL",
+
+    }
+  ];
+  
