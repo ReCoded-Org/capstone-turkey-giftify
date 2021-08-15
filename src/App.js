85cec2c6c253d6038dfa17598f9b608df5ed8b5c
CeyhunGulbas <cetree61@gmail.com>
2021-08-15 03:33:46 +0300

    Build: add about

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About/About"
 
 function App() {
   return (
     <>
       <Container>
+        <About />
         <Footer />
       </Container>
     </>
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,32 +1,37 @@
 import React from "react"
 import "./About.css"
-import "bootstrap/dist/css/bootstrap.min.css";
+import aboutImage from "./aboutImage.png"
+import { Row } from "react-bootstrap";
 
 const About = () => {
 
 
     return(
         <div className="aboutUsMainDiv">
-            <h1>About Us</h1>
-            <div className="visionMisionMainDiv">
-                <p>Our Vision: Everyone can get what they want in a fast and easy way</p>
-                <p>Our Mision: Trade things are no longer used between people for a clean environment</p>
-            </div>
-            <div className="middleArea">
-                <div className="middleAreaTextbox">
-                    <p>
-                    Giftify is a platform for trading things that are no longer 
-                    used between people who stay in the same area. People have 
-                    the chance to get their needs in free by giving the convincing 
-                    reason of needing a specific product/service to the owner then 
-                    the owner has the freedom to choose which applicant has deserved 
-                    his product or service more than the other applicants.
-                    </p>
+            <Row className="upperPartAbout">
+                <h1>About Us</h1>
+                <div className="visionMisionMainDiv">
+                    <span>Our Vision: Everyone can get what they want in a fast and easy way</span>
+                    <span>Our Mision: Trade things are no longer used between people for a clean environment</span>
                 </div>
-                <div className="middleAreaImage">
-
+            </Row>
+            <Row>
+                <div className="middleArea">
+                    <div className="middleAreaTextbox">
+                        <p>
+                        Giftify is a platform for trading things that are no longer 
+                        used between people who stay in the same area. People have 
+                        the chance to get their needs in free by giving the convincing 
+                        reason of needing a specific product/service to the owner then 
+                        the owner has the freedom to choose which applicant has deserved 
+                        his product or service more than the other applicants.
+                        </p>
+                    </div>
+                    <div className="middleAreaImage">
+                        <img src={aboutImage} alt="About pic" />
+                    </div>
                 </div>
-            </div>
+            </Row>
         </div>
         
         
