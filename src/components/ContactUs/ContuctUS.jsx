3684b547967677f26dfa2971c13cf87c4e37fbb8
aysha <ayshe.997@gmail.com>
2021-08-17 09:42:52 +0300

    Build: contuct page

--- src/components/ContactUs/ContuctUS.jsx
+++ src/components/ContactUs/ContuctUS.jsx
@@ -1,45 +1,74 @@
-import React , {useState}from "react";
-import { FiMapPin , FiPhone, FiMail , FiInstagram} from 'react-icons/fi'
-
-import { AiOutlineFacebook } from 'react-icons/ai'
+import React , {useState, useEffect}from "react";
+import { FiMapPin , FiPhone, FiMail} from 'react-icons/fi'
+import { ImFacebook2 } from 'react-icons/im';
+import { GrInstagram } from 'react-icons/gr';
+import db from "../../firebase";
+import "./ContactUs.css";
 
 function ConactUS() {
-
+    const [inputdata, setInputData] = useState([]);
     const [inputName, setInputName] = useState("");
     const [inputEmail, setInputEmail] = useState("");
     const [inputMassege, setInputMassege] = useState("");
 
-  return (
-    <div>
-        <div>
-            
-       
-            <h4> <FiMapPin /> Turkey - Istanbul</h4>
-            
-            <p><FiPhone /> +90 500 000 00 00</p>
-            
-            <p><FiMail /> example@gmail.com</p>
-            
-            <a href = "#"><AiOutlineFacebook /> Giftify</a>
-            <br/>
-           
-           
-            <a href = "#"> <FiInstagram /> Giftify</a>
-            
-        </div>
+    function handleSubmit (e){
+      e.preventDefault();
+      console.log(inputName)
+      db.collection("contactUs").add({
+        name: inputName,
+        email: inputEmail,
+        massege: inputMassege
+      }).then(() => {
+        setInputName("")
+        setInputEmail("")
+        setInputMassege("")
+      })
+    }
 
-        <form>
+  const getData = () => {
+    db.collection("contactUs").onSnapshot(function (querySnapshot){
+      setInputData(querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        name : doc.data().inputName,
+        email: doc.data().inputEmail,
+        massege: doc.data().inputMassege,
+      }))
+      );
+    })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <div className = "container">
+      <h1>Contact Us</h1>
+    <div className = "box">
+        <div className = "contatctinfo">
+            <h5> <FiMapPin className = "icon" /> Turkey - Istanbul</h5>
+            <h5><FiPhone className = "icon" /> +90 500 000 00 00</h5>
+            <h5><FiMail className = "icon" /> giftify@giftify.com</h5>
+            <div className = "sosyalMed">
+            <h5><a   href = "#"><ImFacebook2 className = "facebook" /> Giftify</a></h5>
+            <h5><a  href = "#"> <GrInstagram  className = "instagram"/> Giftify</a>  </h5>
+            </div>
+        </div>
+        <div>
+        <form className = "inpurForm" onSubmit = {handleSubmit} >
+          <h5>Get In Touch</h5>
             <label>
-            Name
-            <input type="text" name="name"  onChange={(e) => setInputName(e.target.value)}
- />
-            Email   
-            <input type="text" name="email" onChange={(e) => setInputEmail(e.target.value)} />
-            Massege
-            <input type="text" name="massege" onChange={(e) => setInputMassege(e.target.value)} />
+            <h6>Name</h6>
+            <input type="text" name="name" value={inputName} onChange={(e) => setInputName(e.target.value)} /> <br/>
+            <h6>Email</h6>
+            <input type="text" name="email" value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} /> <br/>
+            <h6>Massege</h6>
+            <input className = "massege" type="text" name="massege" value={inputMassege} onChange={(e) => setInputMassege(e.target.value)} />
             </label>
-            <input type="submit" value="Submit" />
+            <input className = "submitData" type="submit" value="Submit" />
         </form>
+        </div>
+    </div>
     </div>
   );
 }
