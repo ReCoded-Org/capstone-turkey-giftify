89fd3c96d6c79fd40cdd86764364fe501f132629
CeyhunGulbas <cetree61@gmail.com>
2021-08-17 11:02:11 +0300

    Build: add ourTeam

--- src/components/OurTeam/OurTeamForSmallScreen.jsx
+++ src/components/OurTeam/OurTeamForSmallScreen.jsx
@@ -9,21 +9,8 @@ const OurTeamForLargeScreen = () => {
       <h1>Our Team</h1>
       <div className="teamMembersCardMainDiv">
         {ourTeamData.map((info, index) => {
-            let card = (<Card key={index}>
-                <Card.Img variant="top" src={info.image} />
-                <Card.Body className="CardUpperPart">
-                <Card.Title>{info.name}</Card.Title>
-                <Card.Text>{info.speciality}</Card.Text>
-                </Card.Body>
-                <Card.Body className="CardLowerPart">
-                <Card.Link href={info.linkedInLink} target="_blank">
-                    <Linkedin />
-                </Card.Link>
-                <Card.Link href={info.githubLink} target="_blank">
-                    <Github />
-                </Card.Link>
-                </Card.Body>
-            </Card> )
+            let card = <Card key={index}>{info.name}</Card> 
+            console.log("card: ",card)
             return(index % 2 === 0 ? <div className="row mx-auto">{card}</div> : {card})
         })}
       </div>
@@ -33,18 +20,22 @@ const OurTeamForLargeScreen = () => {
 
 export default OurTeamForLargeScreen;
 
-{/* <Card key={index}>
-    <Card.Img variant="top" src={info.image} />
-    <Card.Body className="CardUpperPart">
-    <Card.Title>{info.name}</Card.Title>
-    <Card.Text>{info.speciality}</Card.Text>
-    </Card.Body>
-    <Card.Body className="CardLowerPart">
-    <Card.Link href={info.linkedInLink} target="_blank">
-        <Linkedin />
-    </Card.Link>
-    <Card.Link href={info.githubLink} target="_blank">
-        <Github />
-    </Card.Link>
-    </Card.Body>
-</Card> */}
+{/* 
+
+<Card key={index}>
+  <Card.Img variant="top" src={info.image} />
+  <Card.Body className="CardUpperPart">
+  <Card.Title>{info.name}</Card.Title>
+  <Card.Text>{info.speciality}</Card.Text>
+  </Card.Body>
+  <Card.Body className="CardLowerPart">
+  <Card.Link href={info.linkedInLink} target="_blank">
+      <Linkedin />
+  </Card.Link>
+  <Card.Link href={info.githubLink} target="_blank">
+      <Github />
+  </Card.Link>
+  </Card.Body>
+</Card> 
+
+*/}
