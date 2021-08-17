7fe0e44380dbf1bc3e616752f58f0958b843e822
busesisi <buse.senturk@outlook.com>
2021-08-17 11:06:46 +0300

    scss changed to css

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./UserProfile.scss";
+import "./UserProfile.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
