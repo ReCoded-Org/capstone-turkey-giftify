4c26808d8b772c83b11fb0ab2a6b968ffbd143d3
aysha <ayshe.997@gmail.com>
2021-08-17 09:53:33 +0300

    Build: contuct page

---
+++ src/components/ContactUs/__tests__/ContactUs.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContactUs from "./../ContactUs";
+import { cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+
+afterEach(cleanup);
+
+it("matches snapshot", () => {
+  const tree = renderer.create(<ContactUs />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
