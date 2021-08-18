ddbf03cebb76c3a8d065d3c0ed4a3569491af5dc
busesisi <buse.senturk@outlook.com>
2021-08-19 01:43:55 +0300

    responsiveness fixed

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./UserProfile.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import db from "../../firebase";
 
 function UserProfile() {
@@ -25,51 +22,22 @@ function UserProfile() {
       {user &&
         user.map((singleUser) => {
           return (
-            <div id="ProfilePage">
-              <div id="navbarCosplay">NAVBAR</div>
-              <div id="userProfile">
-                <div id="upperSection">
-                  <div id="profilePicture">
-                    <img src={singleUser.profileImg} alt="profile" />
-                  </div>
-                  <Container id="myCont">
-                    <Row>
-                      <Col>
-                        <h3>
-                          {singleUser.firstName} {singleUser.lastName}
-                        </h3>
-                      </Col>
-                      <Col>
-                        <button>Edit</button>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <p>{singleUser.email}</p>
-                      </Col>
-                      <Col>
-                        <p>{singleUser.city}</p>
-                      </Col>
-                    </Row>
-                  </Container>
-
-                  {/*
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <hr />
-                  <div id="belowSection">
-                    <h5>About</h5>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-          </div>*/}
-                </div>
+            <div className="profilePage">
+              <div className="profilePic">
+                <img
+                  src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+                  alt="profilePic"
+                />
+              </div>
+              <div className="texts">
+                <h2>
+                  {singleUser.firstName} {singleUser.lastName}
+                </h2>
+                <p>{singleUser.email}</p>
+                <p>{singleUser.city}</p>
+              </div>
+              <div className="editBtn">
+                <button>Edit</button>
               </div>
             </div>
           );
