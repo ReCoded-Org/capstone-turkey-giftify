5d96189f04ef7455ec3ac9fdb00581b97c5b91de
aysha <ayshe.997@gmail.com>
2021-08-19 22:02:22 +0300

    search component

--- src/components/Navbar/__tests__/Navbar.test.js
+++ src/components/Navbar/__tests__/Navbar.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./../Navbar";
 import renderer from "react-test-renderer";
 
-
 it("Navbar rendered correctly", () => {
   const tree = renderer.create(<Navbar />).toJSON();
   expect(tree).toMatchSnapshot();
--- src/components/Navbar/__tests__/NavbarForLargeScreen.test.js
+++ src/components/Navbar/__tests__/NavbarForLargeScreen.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavbarForLargeScreen from "./../NavbarForLargeScreen";
 import renderer from "react-test-renderer";
 
-
 it("NavbarForLargeScreen rendered correctly", () => {
   const tree = renderer.create(<NavbarForLargeScreen />).toJSON();
   expect(tree).toMatchSnapshot();
