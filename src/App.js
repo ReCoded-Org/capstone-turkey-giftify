3f0877019dff50377547fd5f18df71739bd200c5
aysha <ayshe.997@gmail.com>
2021-08-19 19:15:51 +0300

    search bar section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePageSecondPart from "./containers/homePageSecondPart/HomePageSecondPart";
+import { Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./containers/HomePage/HomePage";
+import RequestItem from "./containers/RequestItem/RequestItem";
+
 function App() {
+
   return (
     <>
-      <Container className ="App">
-        <HomePageSecondPart />
+      <Container fluid className="mainContainer">
+      <Router>
+          <Route path="/" component={Navbar} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/request-item/:id" component={RequestItem} />
+          {/* <Route path="/donations" component={Donations} />
+          <Route path="/about" component={About} />
+          <Route path="/contactUs" component={ContactUs} /> */}
+          <Route path="/" component={Footer} />
+        </Router>
+
+        <HomePageSecondPart className ="serachpart"/>
         <Footer />
       </Container>
     </>
