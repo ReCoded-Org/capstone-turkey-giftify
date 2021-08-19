b379098687edffcf821fde807882cf530a8808e7
aysha <ayshe.997@gmail.com>
2021-08-19 19:31:51 +0300

    search bar section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ContactUSPage from "./containers/HomePageSecondPart/HomePageSecondPart";
+import { Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./containers/HomePage/HomePage";
+import RequestItem from "./containers/RequestItem/RequestItem";
+
 
 function App() {
   return (
     <>
-      <Container>
-        <ContactUSPage />
+      <Container fluid className="mainContainer">
+        <Router>
+          <Route path="/" component={Navbar} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/request-item/:id" component={RequestItem} />
+          {/* <Route path="/donations" component={Donations} />
+          <Route path="/about" component={About} />
+          <Route path="/contactUs" component={ContactUs} /> */}
+          <ContactUSPage />    
+          <Route path="/" component={Footer} />
+        </Router>
       </Container>
+
     </>
   );
 }
