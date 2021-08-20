fc023fd3107a646931016d6c4802cbd866b5e4ed
CeyhunGulbas <cetree61@gmail.com>
2021-08-20 11:34:10 +0300

    Fix: change location of files

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,45 +1,8 @@
 import React from "react";
-import "./About.css";
-import aboutImage from "./aboutImage.png";
-import { Row } from "react-bootstrap";
+import AboutComponent from "./../../components/About/AboutComponent";
 
 const About = () => {
-  return (
-    <div className="aboutUsMainDiv">
-      <Row className="upperPartAbout">
-        <h1>About Us</h1>
-        <div className="visionMisionMainDiv">
-          <p>
-            <span className="spanDescription">Our Vision: </span>
-            Everyone can get what they want in a fast and easy way
-          </p>
-          <p>
-            <span className="spanDescription">
-              <br />
-              Our Mision:
-            </span>
-            Trade things are no longer used between people for a clean
-            environment
-          </p>
-        </div>
-      </Row>
-      <Row className="middleArea">
-        <div className="middleAreaTextbox">
-          <p>
-            Giftify is a platform for trading things that are no longer used
-            between people who stay in the same area. People have the chance to
-            get their needs in free by giving the convincing reason of needing a
-            specific product/service to the owner then the owner has the freedom
-            to choose which applicant has deserved his product or service more
-            than the other applicants.
-          </p>
-        </div>
-        <div className="middleAreaImage">
-          <img src={aboutImage} alt="About pic" />
-        </div>
-      </Row>
-    </div>
-  );
+  return <AboutComponent />;
 };
 
 export default About;
