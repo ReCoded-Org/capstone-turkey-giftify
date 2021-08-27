61862fa7a5a20be64c566fb1a79939b05385bf99
CeyhunGulbas <cetree61@gmail.com>
2021-08-28 01:45:21 +0300

    Fix: donatersMessagePagetest bug

--- src/components/RequestsPage/__tests__/DonatersMessagePage.test.js
+++ src/components/RequestsPage/__tests__/DonatersMessagePage.test.js
@@ -5,12 +5,14 @@ import renderer from "react-test-renderer";
 
 afterEach(cleanup);
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: jest
+    .fn()
+    .mockReturnValue({ category: "clothing", id: "3iz39LBgqzm67DAOhPy1" }),
+}));
+
 it("DonatersMessagePage rendered correctly", () => {
   const tree = renderer.create(<DonatersMessagePage />).toJSON();
   expect(tree).toMatchSnapshot();
 });
-
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useParams: jest.fn().mockReturnValue({ environment: 'dev', service: 'fakeService' }),
-}))
