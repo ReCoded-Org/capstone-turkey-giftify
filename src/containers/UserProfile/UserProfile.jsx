d844784e1619dee93d99431a11d82f076a19b0e2
busesisi <buse.senturk@outlook.com>
2021-08-29 13:17:54 +0300

    on progress

--- src/containers/UserProfile/UserProfile.jsx
+++ src/containers/UserProfile/UserProfile.jsx
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./UserProfile.css";
 import db from "../../firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function UserProfile() {
-  // eslint-disable-next-line
   const [edit, setEdit] = useState(false);
 
   function handleClick() {
     setEdit((edit) => !edit);
+  }
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user !== null) {
+    // The user object has basic properties such as display name, email, etc.
+    const displayFirstName = user.firstName;
+    const displayLastName = user.lastName;
+    const email = user.email;
+    const city = user.city;
+    const photoURL = user.photoURL;
+    //const emailVerified = user.emailVerified;
+
+    // The user's ID, unique to the Firebase project. Do NOT use
+    // this value to authenticate with your backend server, if
+    // you have one. Use User.getToken() instead.
+    const uid = user.uid;
   }
 
   //needs work
@@ -15,10 +32,42 @@ function UserProfile() {
     console.log("Data Edited");
   }
 
-  const [user, setUser] = useState([]);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pic, setPic] = useState("");
+  const [city, setCity] = useState("");
+  const [email, setEmail] = useState("");
+  const sub = (e) => {
+    e.preventDefault();
+
+    // Add data to the store
+    db.collection("users")
+      .set({
+        firstName: name,
+        lastName: lastName,
+        city: city,
+        email: email,
+        photoURL: pic,
+      })
+      .then((docRef) => {
+        alert("Data Successfully Submitted");
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+  };
+
+  {
+    /*const [user, setUser] = useState({
+    firstName: name,
+    lastName: lastName,
+    city: city,
+    email: email,
+    photoURL: pic,});*/
+  }
 
   const fetchUser = async () => {
-    const response = db.collection("users");
+    const response = db.collection("users").doc();
     const data = await response.get();
     data.docs.forEach((item) => {
       setUser([...user, { id: item.id, ...item.data() }]);
@@ -34,29 +83,21 @@ function UserProfile() {
   function Card() {
     return (
       <div className="profileRender">
-        {user &&
-          user.map((singleUser) => {
-            return (
-              <div key={singleUser.id} className="profilePage">
-                <div className="profilePic">
-                  <img
-                    src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
-                    alt="profilePic"
-                  />
-                </div>
-                <div className="texts">
-                  <h2>
-                    {singleUser.firstName} {singleUser.lastName}
-                  </h2>
-                  <p>{singleUser.email}</p>
-                  <p>{singleUser.city}</p>
-                </div>
-                <div className="editBtn">
-                  <button onClick={handleClick}>Edit</button>
-                </div>
-              </div>
-            );
-          })}
+        <div key={user.id} className="profilePage">
+          <div className="profilePic">
+            <img src={user.photoURL} alt="profilePic" />
+          </div>
+          <div className="texts">
+            <h2>
+              {user.firstName} {user.lastName}
+            </h2>
+            <p>{user.email}</p>
+            <p>{user.city}</p>
+          </div>
+          <div className="editBtn">
+            <button onClick={handleClick}>Edit</button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -65,7 +106,12 @@ function UserProfile() {
     return (
       <div className="profileRender">
         <div className="profilePage">
-          <form onSubmit={handleSubmit}>
+          <form
+            onSubmit={handleSubmit}
+            onSubmit={(event) => {
+              sub(event);
+            }}
+          >
             <div className="profilePage">
               <input type="file" className="profilePicEntry" />
             </div>
@@ -73,25 +119,49 @@ function UserProfile() {
               <div>
                 <label className="importantText">
                   First Name
-                  <input type="text" />
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
                 </label>
               </div>
               <div>
                 <label className="importantText">
                   Last Name
-                  <input type="text" />
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                  />
                 </label>
               </div>
               <div>
                 <label className="regularText">
                   E-Mail
-                  <input type="email" />
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
                 </label>
               </div>
               <div>
                 <label className="regularText">
                   City
-                  <input type="text" />
+                  <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => {
+                      setCity(e.target.value);
+                    }}
+                  />
                 </label>
               </div>
             </div>
@@ -105,8 +175,6 @@ function UserProfile() {
       </div>
     );
   }
-
-  //conditional rendering
 
   if (edit === true) {
     return <Form />;
