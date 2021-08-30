56bfc1be1cf1407517a49346bb3ebe62eeeaad51
aysha <ayshe.997@gmail.com>
2021-08-30 19:56:26 +0300

    Smooth Scroll in home page

--- src/components/ScrollToTop/__tests__/ScrollToTop.test.js
+++ src/components/ScrollToTop/__tests__/ScrollToTop.test.js
@@ -1,10 +1,3 @@
-import React from "react";
-import ScrollToTop from "../ScrollToTop";
-import { cleanup } from "@testing-library/react";
-import renderer from "react-test-renderer";
-
-// afterEach(cleanup);
-
 it("Smooth Scrolling rendered correctly", () => {
   window.scrollTo = jest.fn();
   afterEach(() => {
