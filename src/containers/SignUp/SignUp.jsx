540c140d1fa02d14c74816d948dbfced2dd4702f
demirbileku <udemirbilek2@gmail.com>
2021-08-31 13:31:30 +0300

    google and facebook login2

--- src/containers/SignUp/SignUp.jsx
+++ src/containers/SignUp/SignUp.jsx
@@ -57,12 +57,7 @@ function SignUp() {
                 photoURL: "",
                 city: "",
               });
-            })
-            .catch((error) => {});
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
+            });
         });
     } else if (formData.password !== formData.password2) {
       event.preventDefault();
@@ -86,9 +81,6 @@ function SignUp() {
       .signInWithPopup(provider)
       .then((result) => {
         /** @type {firebase.auth.OAuthCredential} */
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const { credential } = result;
-        const token = credential.accessToken;
         // The signed-in user info, we create an entry in users to store data in database
         const { user } = result;
         db.collection("users").doc(user.uid).set({
@@ -98,10 +90,6 @@ function SignUp() {
           city: "",
           email: user.email,
         });
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
       });
   };
 
@@ -113,7 +101,6 @@ function SignUp() {
       .signInWithPopup(provider)
       .then((result) => {
         /** @type {firebase.auth.OAuthCredential} */
-        const { credential } = result;
 
         // The signed-in user info, we create an entry in users to store data in database
         const { user } = result;
@@ -124,13 +111,6 @@ function SignUp() {
           city: "",
           email: user.email,
         });
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        const { accessToken } = credential;
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
       });
   };
 
@@ -239,15 +219,6 @@ function SignUp() {
           Sign Up!
         </Button>
 
-        <Button
-          variant="light"
-          type="button"
-          className="button"
-          onClick={logInFacebook}
-        >
-          Log In!
-        </Button>
-
         <Button variant="light" type="button" onClick={logInGoogle}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -259,7 +230,21 @@ function SignUp() {
           >
             <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
           </svg>
-          Log in with Google!
+          Continue with Google!
+        </Button>
+
+        <Button variant="light" type="button" onClick={logInFacebook}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-facebook"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+          </svg>
+          Continue with Facebook!
         </Button>
       </Form>
     </Container>
