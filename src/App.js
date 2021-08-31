05af50aa56017507d2ebb732127236c633ddb714
Ayşe Saflo <81983858+ayshesaflo@users.noreply.github.com>
2021-08-31 13:56:37 +0300

    Contact component (#29)
    * contact page
    
    * Build: contuct page
    
    * Build: contuct page
    
    * Build: contuct page
    
    * Build: contuct page
    
    * finish  contact us page
    
    * finish  contact us page
    
    * finish  contact us page
    
    * did the required changes
    
    * search section
    
    * contact component
    
    * Contact component
    
    * contact us component
    
    * contact us component
    
    * contact us component
    
    * contact us component
    
    * contact us component
    
    * contact us component
    
    * contact us component
    
    * Finish contact us component
    
    Co-authored-by: Ceyhun Gulbas <44509662+ceyhungulbas@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
+import ContactUsPage from "./containers/ContactUsPage/ContactUsPage";
 import AboutUs from "./containers/About/AboutUs";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./containers/HomePage/HomePage";
 import RequestItem from "./containers/RequestItem/RequestItem";
+
 function App() {
   return (
     <>
@@ -16,10 +18,8 @@ function App() {
           <Route path="/" component={Navbar} />
           <Route exact path="/" component={HomePage} />
           <Route exact path="/request-item/:id" component={RequestItem} />
-          {/* <Route path="/requests" component={Requests} />
-          <Route path="/donations" component={Donations} />
-          <Route path="/about" component={About} />
-          <Route path="/contactUs" component={ContactUs} /> */}
+          {/* <Route path="/donations" component={Donations} /> */}
+          <Route path="/contactUs" component={ContactUsPage} />
           <Route exact path="/about" component={AboutUs} />
           <Route path="/" component={Footer} />
         </Router>
---
+++ src/containers/ContactUsPage/ContactUsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactUs from "../../components/ContactUs/ContactUs";
+
+function ContactUsPage() {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default ContactUsPage;
